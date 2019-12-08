5b291d93a59cdceb63d48994abaf78fe8b95c68d
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2019-12-08 23:46:55 +0300

    fix(Core): Omit polyfill added, utility-types removed

--- src/message-response.type.ts
+++ src/message-response.type.ts
@@ -1,6 +1,6 @@
-import { Omit } from "utility-types";
-
 import { Message } from './message.interface';
+
+type Omit<ObjectType, KeysType extends keyof ObjectType> = Pick<ObjectType, Exclude<keyof ObjectType, KeysType>>;
 
 type UncompletedMessageResponse<T = any> = Omit<Message<T>, 'path'> & { completed: false; };
 type CompletedMessageResponse = Omit<Message, 'body' | 'path'> & { completed: true; };
